3b5eaac6ec4d10462755bcb0ba7265a54e9494bd
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-07-04 10:35:37 -0400

    fix cors

--- handler.js
+++ handler.js
@@ -28,6 +28,10 @@ module.exports.hello = async (event) => {
 
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
         body: JSON.stringify(newsArticle)
       }
     });
