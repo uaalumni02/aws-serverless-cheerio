7af399baf2e48696740eb468a4a163af971a1f94
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-07-05 15:36:53 -0400

    add sports function

--- handler.js
+++ handler.js
@@ -36,3 +36,36 @@ module.exports.hello = async (event) => {
       }
     });
 };
+
+module.exports.sports = async (event) => {
+  return axios
+    .get("https://www.nytimes.com/section/sports")
+    .then((response) => {
+      const body = $.load(response.data).html();
+      const listItems = $('ol:nth-child(2)', body);
+      const newsArticle = []; 
+      $(listItems).children('li').each((i, e) => {
+        const article = $(e);
+        const p = $('div>p', article);
+        const anchorTag = $('div>h2', article);
+
+        const link = $('a', anchorTag);
+        const title = $(link).text();
+        const url = $(link).attr('href');
+        const desc = $(p).text();
+        
+        const thisArticle = { title, desc, url };
+        newsArticle.push(thisArticle);
+    
+      });
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(newsArticle)
+      }
+    });
+};
