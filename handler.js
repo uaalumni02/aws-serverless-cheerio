354f6d418ae0b7f8f7384b6b798c1186d9e6513c
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-07-05 15:58:37 -0400

    add science handler

--- handler.js
+++ handler.js
@@ -69,3 +69,36 @@ module.exports.sports = async (event) => {
       }
     });
 };
+
+module.exports.science = async (event) => {
+  return axios
+    .get("https://www.nytimes.com/section/science")
+    .then((response) => {
+      const body = $.load(response.data).html();
+      const listItems = $('ol:nth-child(2)', body);
+      const newsArticle = []; 
+      $(listItems).children('li').each((i, e) => {
+        const article = $(e);
+        const p = $('div>p', article);
+        const anchorTag = $('div>h2', article);
+
+        const link = $('a', anchorTag);
+        const title = $(link).text();
+        const url = $(link).attr('href');
+        const desc = $(p).text();
+        
+        const thisArticle = { title, desc, url };
+        newsArticle.push(thisArticle);
+    
+      });
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(newsArticle)
+      }
+    });
+};
